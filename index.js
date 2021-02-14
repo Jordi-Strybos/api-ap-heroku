ebaf97a32769da02acd8165404e800966d4578bc
Jordi-Strybos <61241877+Jordi-Strybos@users.noreply.github.com>
2021-02-14 11:54:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 
-var originsWhitelist = ['https://dreamy-spence-4b3a09.netlify.app/'];
-
-var corsOptions = {
-  origin: function(origin, callback){
-        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-        callback(null, isWhitelisted);
-  },
-  credentials: true
-};
-
-app.use(cors(corsOptions));
+app.use(
+    cors({
+        credentials: true,
+        origin: 'https://dreamy-spence-4b3a09.netlify.app'
+    })
+);
 app.options('*', cors());
 
 app.get('/', (req, res) => res.send('I modified this API!'));
